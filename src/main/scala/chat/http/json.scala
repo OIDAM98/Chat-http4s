ace03bf1f50a3c09b126c6c4a40562c8bea4be6a
Oscar De Alva <oscar.ivan98@gmail.com>
2020-08-18 17:58:33 -0500

    Added new derivation functions for data types created with Newtype. These are found in GVolpe book with help of Issue #127 found in the book's repo.

--- src/main/scala/chat/http/json.scala
+++ src/main/scala/chat/http/json.scala
@@ -1,17 +1,16 @@
 package chat.http
 
-import cats.effect.IO
 import cats.Applicative
+import dev.profunktor.auth.jwt.JwtToken
 import io.circe._
 import io.circe.generic.semiauto._
 import io.circe.refined._
 import io.estatico.newtype.Coercible
 import io.estatico.newtype.ops._
-import eu.timepit.refined.collection.NonEmpty
-
 import org.http4s.EntityEncoder
 import org.http4s.circe.jsonEncoderOf
 
+import eu.timepit.refined.collection.NonEmpty
 import chat.domain.users._
 import chat.domain.messages.types._
 import chat.domain.users.types._
@@ -21,28 +20,28 @@ import eu.timepit.refined._
 import org.http4s.EntityDecoder
 import dev.profunktor.auth.jwt.JwtToken
 
-
 object json extends JsonCodecs {
   implicit def deriveEntityEncoder[F[_]: Applicative, A: Encoder]: EntityEncoder[F, A] =
     jsonEncoderOf[F, A]
 }
 private[http] trait JsonCodecs {
-  implicit val userNameParamDecoder: Decoder[UserNameParam] =
-    Decoder.decodeString.emap(v => refineV[UserNameRules](v).map(_.coerce[UserNameParam]))
-  implicit val validMessageDecoder: Decoder[ValidMessage] =
-    Decoder.decodeString.emap(v => refineV[NonEmpty](v).map(_.coerce[ValidMessage]))
-  implicit val passwordDecoder: Decoder[PasswordParam] =
-    Decoder.decodeString.emap(v => refineV[NonEmpty](v).map(_.coerce[PasswordParam]))
-  implicit val msgReqDecoder: Decoder[MessageRequest] =
-    Decoder.forProduct2("author", "msg")(MessageRequest.apply)
+  implicit def coercibleDecoder[A: Coercible[B, *], B: Decoder]: Decoder[A] =
+    Decoder[B].map(_.coerce[A])
+
+  implicit def coercibleEncoder[A: Coercible[B, *], B: Encoder]: Encoder[A] =
+    Encoder[B].contramap(_.asInstanceOf[B])
+
+  implicit def coercibleKeyDecoder[A: Coercible[B, *], B: KeyDecoder]: KeyDecoder[A] =
+    KeyDecoder[B].map(_.coerce[A])
+
+  implicit def coercibleKeyEncoder[A: Coercible[B, *], B: KeyEncoder]: KeyEncoder[A] =
+    KeyEncoder[B].contramap[A](_.asInstanceOf[B])
+
   implicit val createUserDecoder: Decoder[CreateUser] = deriveDecoder[CreateUser]
   implicit val loginUserDecoder: Decoder[LoginUser]   = deriveDecoder[LoginUser]
-  implicit val msgReqEncoder: Encoder[MessageRequest] =
-    Encoder.forProduct2[MessageRequest, String, String]("author", "msg") { json =>
-      (json.author.value.value, json.msg.value.value)
-    }
-
-  implicit val userNameEnc: Encoder[UserName] = Encoder.forProduct1("username")(_.value)
-  implicit val tokenEncoder: Encoder[JwtToken] = Encoder.forProduct1("access_token")(_.value)
+  implicit val msgReqDecoder: Decoder[MessageRequest] = deriveDecoder[MessageRequest]
+  implicit val msgReqEncoder: Encoder[MessageRequest] = deriveEncoder[MessageRequest]
+  implicit val userEnc: Encoder[User]                 = deriveEncoder[User]
+  implicit val tokenEncoder: Encoder[JwtToken]        = Encoder.forProduct1("access_token")(_.value)
 
 }
