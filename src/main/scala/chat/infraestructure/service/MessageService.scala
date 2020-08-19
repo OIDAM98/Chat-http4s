aaa9d2935f184d81de693cb4d84ba073200b1be9
Oscar De Alva <oscar.ivan98@gmail.com>
2020-08-19 13:09:30 -0500

    Refactored MessageService into its own package

---
+++ src/main/scala/chat/infraestructure/service/MessageService.scala
@@ -0,0 +1,23 @@
+package chat.infraestructure.service
+
+import chat.domain.messages._
+
+import cats.Monad
+import cats.effect.Sync
+import cats.implicits._
+import chat.algebra.MessagesAlgebra
+
+class MessageService[F[_]: Sync: Monad](storage: MessagesAlgebra[F]) {
+  def create(msgReq: MessageRequest): F[Message] = {
+    val msg = msgReq.toDomain()
+    storage.create(msg)
+  }
+  def getAll: F[List[Message]] = storage.getAll
+
+  def getBy(author: AuthorFilter): F[List[Message]] = storage.getBy(author.by)
+}
+
+object MessageService {
+  def empty[F[_]: Sync: Monad](storage: MessagesAlgebra[F]): F[MessageService[F]] =
+    Sync[F].delay(new MessageService[F](storage))
+}
