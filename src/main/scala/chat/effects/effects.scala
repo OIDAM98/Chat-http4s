8e17635a639dc3c9eb16512ac92215fc410c58bb
Oscar De Alva <oscar.ivan98@gmail.com>
2020-08-19 12:39:49 -0500

    Added new Error effects for further validation. Code taken from GVolpe's Pfp in Scala book.

--- src/main/scala/chat/effects/effects.scala
+++ src/main/scala/chat/effects/effects.scala
@@ -1,7 +1,18 @@
 package chat
 
-import cats.{ ApplicativeError, MonadError }
+import cats.{ApplicativeError, MonadError}
 
 package object effects {
   type MonadThrow[F[_]] = MonadError[F, Throwable]
+
+  object MonadThrow {
+    def apply[F[_]](implicit ev: MonadError[F, Throwable]): MonadThrow[F] = ev
+  }
+
+  type ApThrow[F[_]] = ApplicativeError[F, Throwable]
+
+  object ApThrow {
+    def apply[F[_]](implicit ev: ApplicativeError[F, Throwable]): ApThrow[F] = ev
+  }
+
 }
