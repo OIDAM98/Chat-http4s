3f7424ab14dd8a4784079c66494a0fe9b57053f3
Oscar De Alva <oscar.ivan98@gmail.com>
2020-08-19 12:48:23 -0500

    Added find function in UsersAlgebra

--- src/main/scala/chat/algebras/UsersAlgebra.scala
+++ src/main/scala/chat/algebras/UsersAlgebra.scala
@@ -1,7 +1,9 @@
 package chat.algebras
 
 import chat.domain.users.types._
+import chat.domain.users.User
 
 trait UsersAlgebra[F[_]] {
   def create(username: UserName, password: Password): F[UserId]
+  def find(username: UserName, password: Password): F[Option[User]]
 }
