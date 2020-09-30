d95f76df10ca29b6012ebca8d59ab0fd2a319084
Oscar De Alva <oscar.ivan98@gmail.com>
2020-09-30 12:57:38 -0500

    Add Algebra to search users' tokens

--- src/main/scala/chat/algebras/AuthAlgebra.scala
+++ src/main/scala/chat/algebras/AuthAlgebra.scala
@@ -3,9 +3,14 @@ package chat.algebras
 import chat.domain.users.UserName
 import chat.domain.users.Password
 import dev.profunktor.auth.jwt.JwtToken
+import pdi.jwt.JwtClaim
 
 trait AuthAlgebra[F[_]] {
   def newUser(username: UserName, password: Password): F[JwtToken]
   def login(username: UserName, password: Password): F[JwtToken]
   def logout(token: JwtToken, username: UserName): F[Unit]
+}
+
+trait UsersAuth[F[_], A] {
+  def findUser(token: JwtToken)(claim: JwtClaim): F[Option[A]]
 }
